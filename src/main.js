862bc2bd64ae79f067647fd883b1e4b97c92e48e
Alec Fox <amf133@uclive.ac.nz>
2021-05-27 21:26:08 +1200

    Fixed incorrect url

--- src/main.js
+++ src/main.js
@@ -68,6 +68,6 @@ const router = createRouter({
 const app = createApp(App);
 app.use(router);
 app.use(ElementPlus);
-axios.defaults.baseURL = "http://152.67.96.30:4941/api/v1"
+axios.defaults.baseURL = "http://152.67.97.30:4941/api/v1"
 app.config.globalProperties.axios = axios;
 app.mount('#app');
